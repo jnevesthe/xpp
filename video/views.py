b83c690b26c5f1d6d3f2b89882892e3b329919f7
jnevesthe <nevesjosemar9@gmail.com>
2026-07-15 22:49:55 +0100

    Update views.py

--- video/views.py
+++ video/views.py
@@ -1,5 +1,4 @@
-def lastmod(self, obj):
-    return obj.criado_emfrom django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Categoria, Video
 
